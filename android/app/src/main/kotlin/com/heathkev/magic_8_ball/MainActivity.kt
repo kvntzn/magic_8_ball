60d538cb8b168b825ae8d415e8750b06e8992dff
heathkev <kevintuazon45@gmail.com>
2020-09-14 11:52:19 +0800

    initial commit

---
+++ android/app/src/main/kotlin/com/heathkev/magic_8_ball/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.heathkev.magic_8_ball
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: BallPage(),
+    ),
+  );
+}
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade900,
+        title: Text('Ask me anything'),
+      ),
+      body: Container(),
+    )
+  }
+}
