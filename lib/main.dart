47cfd16067d9f32d114c8231f7948a89ce4b892a
heathkev <kevintuazon45@gmail.com>
2020-09-14 12:23:51 +0800

    added images Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,7 +17,25 @@ class BallPage extends StatelessWidget {
         backgroundColor: Colors.blue.shade900,
         title: Text('Ask me anything'),
       ),
-      body: Container(),
-    )
+      body: Ball(),
+    );
   }
 }
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Image(
+        image: AssetImage('images/ball1.png'),
+      ),
+    );
+  }
+}
+
+
